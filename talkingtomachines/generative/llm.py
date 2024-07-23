be4baf830bc66a942457b924e3a0368e360e08db
raymondlow <iamraymondlow@gmail.com>
2024-07-23 10:56:29 +0100

    Included doc strings for query_llm and query_open_ai functions.

--- talkingtomachines/generative/llm.py
+++ talkingtomachines/generative/llm.py
@@ -5,7 +5,16 @@ openai_client = OpenAI()
 
 
 def query_llm(model_info: str, message_history: List[dict]) -> str:
-    """ """
+    """ 
+    Queries a LLM for a response based on the latest message history.
+
+    Parameters:
+        model_info (str): Information about the model.
+        message_history (List[dict]): Contains the history of message exchanged between user and assistant.
+
+    Returns:
+        str: Response from the LLM.
+    """
     if model_info in ["gpt-4o", "gpt-4o-mini", "gpt-4-turbo", "gpt-4", "gpt-3.5-turbo"]:
         return query_open_ai(model_info=model_info, message_history=message_history)
     else:
@@ -15,7 +24,16 @@ def query_llm(model_info: str, message_history: List[dict]) -> str:
 
 
 def query_open_ai(model_info: str, messsage_history: List[dict]) -> str:
-    """Query OpenAI API with the provided prompt."""
+    """
+    Query OpenAI API with the provided prompt.
+    
+    Parameters:
+        model_info (str): Information about the model.
+        message_history (List[dict]): Contains the history of message exchanged between user and assistant.
+
+    Returns:
+        str: Response from the LLM.
+    """
     try:
         response = openai_client.chat.completions.create(
             model=model_info, messages=messsage_history
