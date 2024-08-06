7436306e81e31c05f0f941ea77c518b77b31ae97
raymondlow <iamraymondlow@gmail.com>
2024-08-06 12:39:44 +0100

    Implemented Experiment base class and AIConversationalExperiment class. Implemented treatment assignment strategies (e.g., simple random, complete random, full factorial, block random, and cluster random) at the individual level.

--- talkingtomachines/management/experiment.py
+++ talkingtomachines/management/experiment.py
@@ -1,10 +1,26 @@
-import random
+from typing import Any
 import pandas as pd
-from typing import List
-from talkingtomachines.generative.synthetic_agent import (
-    ConversationalSyntheticAgent,
-    DemographicInfo,
+import datetime
+import base64
+from typing import List, Dict
+from talkingtomachines.generative.synthetic_agent import ConversationalSyntheticAgent
+from talkingtomachines.management.treatment import (
+    simple_random_assignment,
+    complete_random_assignment,
+    full_factorial_assignment,
+    block_random_assignment,
+    cluster_random_assignment,
 )
+
+SUPPORTED_MODELS = ["gpt-4o", "gpt-4o-mini", "gpt-4-turbo", "gpt-4", "gpt-3.5-turbo"]
+SUPPORTED_ASSIGNMENT_STRATEGIES = [
+    "simple_random",
+    "complete_random",
+    "full_factorial",
+    "block_randomisation",
+    "cluster_randomisation",
+    "manual",
+]
 
 
 class Experiment:
@@ -17,14 +33,18 @@ class Experiment:
     def __init__(self):
         self.experiment_id = self.generate_experiment_id()
 
-    def generate_experiment_id(self) -> int:
-        """Generates a unique ID for the experiment
+    def generate_experiment_id(self) -> str:
+        """Generates a unique ID for the experiment by concatenating the date and time information and encoding it.
 
         Returns:
-            int: Unique ID for the experiment.
+            str: Unique ID for the experiment as a base64 encoded string.
         """
-        # TODO
-        return 0
+        current_datetime = datetime.datetime.now()
+        current_datetime_str = current_datetime.strftime("%Y%m%d%H%M%S%f")
+        encoded_bytes = base64.urlsafe_b64encode(current_datetime_str.encode("utf-8"))
+        experiment_id = encoded_bytes.decode("utf-8")
+
+        return experiment_id
 
     def get_experiment_id(self) -> str:
         """Return the experiment ID of the synthetic agent.
@@ -36,17 +56,30 @@ class Experiment:
 
 
 class AIConversationalExperiment(Experiment):
-    """A class for constructing AI-based conversational experiments. Inherits from the Experiment base class.
+    """A class representing an AI conversational experiment. Inherits from the Experiment base class.
+
+    This class extends the base `Experiment` class and provides additional functionality
+    specific to AI conversational experiments.
 
     Args:
-        model_info (str): The information about the model used in this conversational experiment.
-        experiment_context (str): The context of the experiment.
+        model_info (str): The information about the AI model used in the experiment.
+        experiment_context (str): The context or purpose of the experiment.
+        agent_demographics (pd.DataFrame): The demographic information of the agents participating in the experiment.
+        treatments (dict[str, Any], optional): The treatments for the experiment. Defaults to an empty dictionary.
+        treatment_assignment_strategy (str, optional): The strategy used for assigning treatments to agents.
+            Defaults to "simple_random".
+
+    Raises:
+        ValueError: If the provided model_info is not supported.
+        ValueError: If the provided treatment_assignment_strategy is not supported.
+        ValueError: If the provided agent_demographics is an empty DataFrame.
 
     Attributes:
-        model_info (str): The information about the model used in this conversational experiment.
-        experiment_context (str): The context of the experiment.
-        experiment_id (str): The unique ID of the experiment.
-        synthetic_agents (List[ConversationalSyntheticAgent]): The list of synthetic agents generated for this experiment.
+        model_info (str): The information about the AI model used in the experiment.
+        experiment_context (str): The context or purpose of the experiment.
+        agent_demographics (pd.DataFrame): The demographic information of the agents participating in the experiment.
+        treatments (dict[str, Any]): The treatments for the experiment.
+        treatment_assignment_strategy (str): The strategy used for assigning treatments to agents.
     """
 
     def __init__(
@@ -54,24 +87,82 @@ class AIConversationalExperiment(Experiment):
         model_info: str,
         experiment_context: str,
         agent_demographics: pd.DataFrame,
-        treatments: List[str] = [],
-        treatment_assignment_strategy: str = "random",
-        num_sessions: int = 10,
+        treatments: dict[str, Any] = {},
+        treatment_assignment_strategy: str = "simple_random",
     ):
         super().__init__()
-        # TODO need to ensure that model_info is supported
-        self.model_info = model_info
-        self.experiment_context = experiment_context
 
-        # TODO need to ensure that num_session is >0
-        self.num_sessions = num_sessions
+        self.model_info = self.check_model_info(model_info)
+        self.treatment_assignment_strategy = self.check_treatment_assignment_strategy(
+            treatment_assignment_strategy
+        )
+        self.agent_demographics = self.check_agent_demographics(agent_demographics)
+        self.experiment_context = experiment_context
         self.treatments = treatments
 
-        # TODO need to ensure that treatment assignment strategy is supported: random
-        self.treatment_assignment_strategy = treatment_assignment_strategy
+        # agent_demographics_with_treatment, treatment_assignment = self.assign_treatment()
+        # self.agent_demographics_with_treatment = agent_demographics_with_treatment
+        # self.treatment_assignment = treatment_assignment
 
-        # TODO need to ensure that agent demographics is not empty
-        self.agent_demographics = agent_demographics
+    def check_model_info(self, model_info: str) -> str:
+        """Checks if the provided model_info is supported.
+
+        Args:
+            model_info (str): The model_info to be checked.
+
+        Returns:
+            str: The validated model_info.
+
+        Raises:
+            ValueError: If the provided model_info is not supported.
+        """
+        if model_info not in SUPPORTED_MODELS:
+            raise ValueError(
+                f"Unsupported model_info: {model_info}. Supported models are: {SUPPORTED_MODELS}."
+            )
+        else:
+            return model_info
+
+    def check_treatment_assignment_strategy(
+        self, treatment_assignment_strategy: str
+    ) -> str:
+        """Checks if the provided treatment_assignment_strategy is supported.
+
+        Args:
+            treatment_assignment_strategy (str): The treatment_assignment_strategy to be checked.
+
+        Returns:
+            str: The validated treatment_assignment_strategy.
+
+        Raises:
+            ValueError: If the provided treatment_assignment_strategy is not supported.
+        """
+
+        if treatment_assignment_strategy not in SUPPORTED_ASSIGNMENT_STRATEGIES:
+            raise ValueError(
+                f"Unsupported treatment_assignment_strategy: {treatment_assignment_strategy}. Supported strategies are: {SUPPORTED_ASSIGNMENT_STRATEGIES}."
+            )
+        # if treatment_assignment_stratey == "manual" and "treatment" not in self.agent_demographics.columns:
+        #     raise ValueError("Column 'treatment' not found in agent_demographics for manual treatment assignment.")
+        else:
+            return treatment_assignment_strategy
+
+    def check_agent_demographics(self, agent_demographics: str) -> str:
+        """Checks if the provided agent_demographics is empty.
+
+        Args:
+            agent_demographics (str): The agent_demographics to be checked.
+
+        Returns:
+            str: The validated agent_demographics.
+
+        Raises:
+            ValueError: If the provided agent_demographics is an empty dataframe.
+        """
+        if agent_demographics.empty:
+            raise ValueError("agent_demographics DataFrame cannot be empty.")
+        else:
+            return agent_demographics
 
     def get_model_info(self) -> str:
         """Return the model used in this experiment.
@@ -89,19 +180,19 @@ class AIConversationalExperiment(Experiment):
         """
         return self.experiment_context
 
-    def get_num_sessions(self) -> int:
-        """Return the number of sessions that will be generated for this experiment.
+    def get_agent_demographics(self) -> pd.DataFrame:
+        """Return the agents' demographic information for this experiment.
 
         Returns:
-            str: The number of experimental sessions.
+            pd.DataFrame: The agents' demographic information.
         """
-        return self.num_sessions
+        return self.agent_demographics
 
-    def get_treatments(self) -> List[str]:
+    def get_treatments(self) -> dict[str, Any]:
         """Return the treatments for this experiment.
 
         Returns:
-            str: The treatments assigned.
+            dict[str, Any]: The treatments for this experiment.
         """
         return self.treatments
 
@@ -113,67 +204,46 @@ class AIConversationalExperiment(Experiment):
         """
         return self.treatment_assignment_strategy
 
-    def get_agent_demographics(self) -> pd.DataFrame:
-        """Return the agents' demographic information for this experiment.
+    # def assign_treatment(self) -> Dict[str, List[ConversationalSyntheticAgent]]:
+    #     """Assign treatments to agents based on the specified treatment assignment strategy.
 
-        Returns:
-            str: The agents' demographic information.
-        """
-        return self.agent_demographics
+    #     Returns:
+    #         dict: A dictionary where keys are treatment names and values are lists of agents assigned to those treatments.
+    #     """
+    #     if self.treatment_assignment_strategy == "simple_random":
+    #         return simple_random_assignment()
+    #     elif self.treatment_assignment_strategy == "complete_random":
+    #         return complete_random_assignment()
+    #     elif self.treatment_assignment_strategy == "full_factorial":
+    #         return full_factorial_assignment()
+    #     elif self.treatment_assignment_strategy == "block_randomisation":
+    #         return block_random_assignment()
+    #     elif self.treatment_assignment_strategy == "cluster_randomisation":
+    #         return cluster_random_assignment()
+    #     else:
+    #         raise ValueError(f"Unsupported treatment_assignment_strategy: {self.treatment_assignment_strategy}. Supported strategies are: {self.SUPPORTED_ASSIGNMENT_STRATEGIES}.")
 
-    def generate_session_id(self) -> int:
-        """Generates a unique session ID to be assigned to a synthetic agent.
+    # def generate_agent(self, session_id) -> ConversationalSyntheticAgent:
+    #     """Generates a synthetic conversational agent based on the demographic information provided.
 
-        Returns:
-            int: Session ID for a synthetic agent.
-        """
-        # TODO
-        return 0
+    #     Returns:
+    #         ConversationalSyntheticAgent: A ConversationalSyntheticAgent object generated for this experiment.
+    #     """
+    #     if self.get_treatment_assignment == "random":
+    #         treatment = self.random_treatment_assignment()
+    #     else:
+    #         treatment = ""
 
-    def random_treatment_assignment(self) -> str:
-        """Randomly selects one of the treatments.
+    #     agent = ConversationalSyntheticAgent(
+    #         experiment_id=self.get_experiment_id(),
+    #         experiment_context=self.get_experiment_context(),
+    #         session_id=self.generate_session_id(),
+    #         demographic_info=self.sample_agent_demographic(),
+    #         model_info=self.get_model_info(),
+    #         assigned_treatment=treatment,
+    #     )
 
-        Returns:
-            str: Selected treatment arm.
-        """
-        treatment_list = self.get_treatments()
-
-        if len(treatment_list) == 0:
-            return ""
-        else:
-            return random.choice(treatment_list)
-
-    def sample_agent_demographic(self) -> DemographicInfo:
-        """Randomly samples a demographic profile for synthetic agent.
-
-        Returns:
-            DemographicInfo: Randomly sampled demographic profile for synthetic agent.
-        """
-        agent_demographics = self.get_agent_demographics()
-        random_demographic = agent_demographics.sample(n=1)
-        return random_demographic.to_dict(orient="records")[0]
-
-    def generate_conversational_agent(self, session_id) -> ConversationalSyntheticAgent:
-        """Generates a synthetic conversational agent based on the demographic information provided.
-
-        Returns:
-            ConversationalSyntheticAgent: A ConversationalSyntheticAgent object generated for this experiment.
-        """
-        if self.get_treatment_assignment == "random":
-            treatment = self.random_treatment_assignment()
-        else:
-            treatment = ""
-
-        agent = ConversationalSyntheticAgent(
-            experiment_id=self.get_experiment_id(),
-            experiment_context=self.get_experiment_context(),
-            session_id=self.generate_session_id(),
-            demographic_info=self.sample_agent_demographic(),
-            model_info=self.get_model_info(),
-            assigned_treatment=treatment,
-        )
-
-        return agent
+    #     return agent
 
     # def run_experiment(experiment_type: str, experiment_data: dict) -> dict:
     #     """Run an experiment of the specified type."""
--- talkingtomachines/management/treatment.py
+++ talkingtomachines/management/treatment.py
@@ -1,9 +1,169 @@
-def assign_treatment(participant_data: dict) -> dict:
-    """Assign treatment based on participant data."""
-    try:
-        # Implement treatment assignment functionality
-        pass
-    except Exception as e:
-        # Log the exception
-        print(f"Error during treatment assignment: {e}")
-        return {}
+import random
+import pandas as pd
+from talkingtomachines.generative.synthetic_agent import DemographicInfo
+from typing import List, Any, Tuple
+from itertools import product
+
+
+def simple_random_assignment(
+    treatment_labels: List[str], agent_demographics: pd.DataFrame
+) -> Tuple[pd.DataFrame, dict[str, List]]:
+    """Randomly assigns agents to different treatments based on their demographics.
+
+    Args:
+        treatment_labels (List[str]): A list containing the treatment labels.
+        agent_demographics (pd.DataFrame): A pandas DataFrame containing the demographic information of the agents.
+
+    Returns:
+        Tuple[pd.DataFrame, dict[str, List]]: A tuple containing the updated agent demographics DataFrame and a dictionary
+        mapping treatment labels to lists of agent IDs assigned to each treatment.
+    """
+    num_agents = len(agent_demographics)
+    agent_demographics["treatment"] = [
+        random.choice(treatment_labels) for _ in range(num_agents)
+    ]
+
+    treatment_assignment = {}
+    for label in treatment_labels:
+        treatment_assignment[label] = agent_demographics[
+            agent_demographics["treatment"] == label
+        ]["ID"].tolist()
+
+    return agent_demographics, treatment_assignment
+
+
+def complete_random_assignment(
+    treatment_labels: List[Any],
+    agent_demographics: pd.DataFrame,
+) -> Tuple[pd.DataFrame, dict[Any, List]]:
+    """Randomly assigns agents to different treatments in a round robin fashion to ensure equal distribution of subjects across all treatments.
+
+    Args:
+        treatment_labels (List[Any]): A list of treatment labels.
+        agent_demographics (pd.DataFrame): A DataFrame containing agent demographics.
+
+    Returns:
+        Tuple[pd.DataFrame, dict[Any, List]]: A tuple containing the updated agent demographics DataFrame and a dictionary
+        mapping treatment labels to lists of agent IDs assigned to each treatment.
+    """
+    num_agents = len(agent_demographics)
+    num_treatments = len(treatment_labels)
+    agent_demographics["treatment"] = [
+        treatment_labels[i % num_treatments] for i in range(num_agents)
+    ]
+
+    treatment_assignment = {}
+    for label in treatment_labels:
+        treatment_assignment[label] = agent_demographics[
+            agent_demographics["treatment"] == label
+        ]["ID"].tolist()
+
+    return agent_demographics, treatment_assignment
+
+
+def full_factorial_assignment(
+    treatment_labels: List[List[str]],
+    agent_demographics: pd.DataFrame,
+) -> Tuple[pd.DataFrame, dict[str, List]]:
+    """Assigns treatments to agents using a full factorial design.
+
+    This function takes a list of treatment labels and a DataFrame containing agent demographics,
+    and assigns treatments to agents using a full factorial design. It generates all possible combinations
+    of treatment labels and assigns agents to each combination randomly.
+
+    Args:
+        treatment_labels (List[List[str]]): A list of treatment labels. Each inner list represents the possible
+            treatment options for a specific factor.
+        agent_demographics (pd.DataFrame): A DataFrame containing agent demographics.
+
+    Returns:
+        Tuple[pd.DataFrame, dict[str, List]]: A tuple containing the updated agent demographics DataFrame and a dictionary
+        mapping treatment labels to lists of agent IDs assigned to each treatment.
+    """
+    treatment_label_combinations = list(product(*treatment_labels))
+
+    return complete_random_assignment(treatment_label_combinations, agent_demographics)
+
+
+def block_random_assignment(
+    treatment_labels: List[str], agent_demographics: pd.DataFrame, block_size: int
+) -> Tuple[pd.DataFrame, dict[str, List]]:
+    """Assigns treatments randomly to agents using block random assignment.
+
+    Args:
+        treatment_labels (List[str]): A list of treatment labels.
+        agent_demographics (pd.DataFrame): A DataFrame containing agent demographics.
+        block_size (int): The size of each block.
+
+    Returns:
+        Tuple[pd.DataFrame, dict[str, List]]: A tuple containing the updated agent demographics DataFrame and a dictionary mapping block-treatment assignments to agent IDs.
+    """
+    num_agents = len(agent_demographics)
+    num_treatments = len(treatment_labels)
+    num_blocks = num_agents // block_size
+
+    # Assign block to subjects
+    block_assignments = [i % num_blocks for i in range(num_agents)]
+    random.shuffle(block_assignment)
+    agent_demographics["block"] = block_assignments
+
+    # Assign treatments within each block
+    agent_demographics.sort_values(by="block", inplace=True)
+    agent_demographics["treatment"] = [
+        treatment_labels[i % num_treatments] for i in range(num_agents)
+    ]
+
+    block_treatment_assignment = {}
+    for block_assignment, treatment_assignment in list(
+        product(set(block_assignments), treatment_labels)
+    ):
+        block_treatment_assignment[(block_assignment, treatment_assignment)] = (
+            agent_demographics[
+                (agent_demographics["block"] == block_assignment)
+                & (agent_demographics["treatment"] == treatment_assignment)
+            ]["ID"].tolist()
+        )
+
+    return agent_demographics, block_treatment_assignment
+
+
+def cluster_random_assignment(
+    treatment_labels: List[str], agent_demographics: pd.DataFrame, cluster_criteria: str
+) -> Tuple[pd.DataFrame, dict[str, List]]:
+    """Assigns treatments randomly in a round robin fashion to clusters based on specified cluster criteria.
+
+    Args:
+        treatment_labels (List[str]): A list of treatment labels.
+        agent_demographics (pd.DataFrame): A pandas DataFrame containing agent demographics.
+        cluster_criteria (str): The column name in `agent_demographics` DataFrame to use for clustering.
+
+    Returns:
+        Tuple[pd.DataFrame, dict[str, List]]: A tuple containing the updated `agent_demographics` DataFrame
+        with assigned treatments, and a dictionary mapping clusters to assigned treatments.
+    """
+    clusters = list(agent_demographics[cluster_criteria].unique())
+    random.shuffle(clusters)
+
+    num_clusters = len(clusters)
+    num_treatments = len(treatment_labels)
+
+    agent_demographics["treatment"] = None
+    cluster_treatment_assignment = {}
+    assigned_cluster_treatment = [
+        (clusters[i], treatment_labels[i % num_treatments]) for i in range(num_clusters)
+    ]
+    for cluster, treatment in assigned_cluster_treatment:
+        agent_demographics.loc[
+            agent_demographics[cluster_criteria] == cluster, "treatment"
+        ] = treatment
+        cluster_treatment_assignment[(cluster, treatment)] = agent_demographics[
+            agent_demographics[cluster_criteria] == cluster
+        ]["ID"].tolist()
+
+    # Check for None values in treatment column
+    if agent_demographics["treatment"].isnull().any():
+        raise ValueError(
+            "Some agents have not been assigned a treatment when performing cluster random assignment."
+        )
+
+    return agent_demographics, cluster_treatment_assignment
